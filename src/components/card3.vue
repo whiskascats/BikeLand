8483314c64fcb1dd98eeadaf9bd26f7e4b19844a
Whiskas <toby010203222@gmail.com>
2023-02-04 00:36:41 +0800

    M版調整

--- src/components/card3.vue
+++ src/components/card3.vue
@@ -2,13 +2,13 @@
   <div class="col-lg-12 col-md-5 col-12 cards card3 d-flex flex-wrap">
     <div class="card3-img mt-lg-4 default" ref="backgroundImage" style="" @click="cardClick"></div>
     <div class="col-12 d-flex">
-      <h2 class="col-9 my-2" @click="cardClick">{{ item.ScenicSpotName }}</h2>
-      <div class="col-3 locate">
+      <h2 class="col-12 my-2" @click="cardClick">{{ item.ScenicSpotName }}</h2>
+      <!-- <div class="col-3 locate">
         <i class="fas fa-map-marker-alt"></i>
         <div>{{ item.City }}</div>
-      </div>
+      </div> -->
     </div>
-    <div class="col-12 d-flex mb-2 tabs d-sm-none">
+    <div class="col-12 d-none d-lg-flex mb-2 tabs">
       <template v-for="num in 5" :key="num">
         <span class="oneline" v-if="item['Class'+num]">
           {{ item['Class'+num] }}
